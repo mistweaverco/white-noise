eb551ddcef542dd674b61d9f296cffa1f05076e9
Marco Kellershoff <marco@mistweaverco.com>
2025-11-06 16:08:53 +0100

    feat(ui): more zen-like - more subtle active state

--- web/src/routes/(root)/+page.svelte
+++ web/src/routes/(root)/+page.svelte
@@ -2,14 +2,13 @@
 	import '@fortawesome/fontawesome-free/css/all.css';
 	import HeadComponent from '$lib/HeadComponent.svelte';
 	import AudioComponent from '$lib/AudioComponent.svelte';
-  import {getCommitDate, getCommitHash, getCommitHashShort, getRepoURL} from '$lib';
+	import { getCommitDate, getCommitHash, getCommitHashShort, getRepoURL } from '$lib';
 </script>
 
 <HeadComponent
 	data={{
 		title: 'White Noise',
-		description:
-			'A white noise generator application to help you focus, relax, and sleep better.',
+		description: 'A white noise generator application to help you focus, relax, and sleep better.'
 	}}
 />
 
@@ -19,13 +18,18 @@
 			<img src="/logo.svg" alt="White Noise logo" class="m-5 mx-auto w-64" />
 			<h1 class="text-5xl font-bold">White Noise</h1>
 			<p class="py-6">
-        White Noise is a simple application that generates soothing white noise to help
-        you focus, relax, and sleep better.
+				White Noise is a simple application that generates soothing white noise to help you focus,
+				relax, and sleep better.
 			</p>
-      <AudioComponent />
-      <div class="mt-10">
-        <p class="text-sm text-gray-500">📌 <a class="link" href="{getRepoURL()}/commit/{getCommitHash()}">{getCommitHashShort()}</a> 📅 {getCommitDate()}</p>
-      </div>
+			<AudioComponent />
+			<div class="mt-10">
+				<p class="text-sm text-gray-500">
+					📌 <a class="link" href="{getRepoURL()}/commit/{getCommitHash()}"
+						>{getCommitHashShort()}</a
+					>
+					📅 {getCommitDate()}
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
